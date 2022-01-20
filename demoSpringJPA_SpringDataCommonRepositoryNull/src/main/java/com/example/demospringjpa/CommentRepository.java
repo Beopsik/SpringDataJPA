edb92edff5f3ce7fb182e3d58eb991c96eac8872
BeobSik <tjswl756@naver.com>
2022-01-20 13:13:52 +0900

    FEAT: 스프링 데이터 Common 3. Null 처리

---
+++ demoSpringJPA_SpringDataCommonRepositoryNull/src/main/java/com/example/demospringjpa/CommentRepository.java
@@ -0,0 +1,4 @@
+package com.example.demospringjpa;
+
+public interface CommentRepository extends MyRepository<Comment, Long>{
+}
---
+++ demoSpringJPA_SpringDataCommonRepositoryNull/src/test/java/com/example/demospringjpa/CommentRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.example.demospringjpa;
+
+import org.checkerframework.checker.units.qual.C;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CommentRepositoryTest {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Test
+    public void crud(){
+        // Given
+        Comment comment=new Comment();
+        comment.setComment("Hello Comment");
+        commentRepository.save(comment);
+
+        // When
+        List<Comment> all=commentRepository.findAll();
+
+        // Then
+        assertThat(all.size()).isEqualTo(1);
+
+        // When
+        Optional<Comment> byId = commentRepository.findById(100L);
+
+        // Then
+        assertThat(byId).isEmpty();
+
+
+    }
+
+}
