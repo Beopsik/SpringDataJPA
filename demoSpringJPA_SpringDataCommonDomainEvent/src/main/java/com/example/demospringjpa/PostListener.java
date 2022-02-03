260998e9a726cf53dedf68eabcf795e8acad8b10
BeobSik <tjswl756@naver.com>
2022-02-03 10:30:02 +0900

    FEAT: 스프링 데이터 Common 9. 도메인 이벤트_이벤트 리스너를 설정에 바로 등록하는 법

--- demoSpringJPA_SpringDataCommonDomainEvent/src/main/java/com/example/demospringjpa/PostListener.java
+++ demoSpringJPA_SpringDataCommonDomainEvent/src/main/java/com/example/demospringjpa/PostListener.java
@@ -5,10 +5,10 @@ import org.springframework.context.event.EventListener;
 
 public class PostListener{
 
-    @EventListener
-    public void onApplicationEvent(PostPublishedEvent event) {
-        System.out.println("----------------------");
-        System.out.println(event.getPost().getTitle()+" is published");
-        System.out.println("----------------------");
-    }
+//    @EventListener
+//    public void onApplicationEvent(PostPublishedEvent event) {
+//        System.out.println("----------------------");
+//        System.out.println(event.getPost().getTitle()+" is published");
+//        System.out.println("----------------------");
+//    }
 }
