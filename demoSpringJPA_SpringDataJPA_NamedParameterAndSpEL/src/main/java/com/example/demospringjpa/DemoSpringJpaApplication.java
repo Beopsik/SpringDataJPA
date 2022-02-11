87204e1ee2385356c8e9e8127fb78e0a77d7dff2
BeobSik <tjswl756@naver.com>
2022-02-11 11:33:26 +0900

    FEAT: 스프링 데이터 JPA 5. Named Parameter와 SpEL

---
+++ demoSpringJPA_SpringDataJPA_NamedParameterAndSpEL/src/main/java/com/example/demospringjpa/DemoSpringJpaApplication.java
@@ -0,0 +1,15 @@
+package com.example.demospringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class DemoSpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringJpaApplication.class, args);
+
+    }
+
+}
