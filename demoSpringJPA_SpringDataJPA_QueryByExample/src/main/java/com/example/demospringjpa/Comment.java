fc4ee4b807f3e261faafcfbf89510b57a384a894
BeobSik <tjswl756@naver.com>
2022-02-14 11:41:51 +0900

    FEAT:  스프링 데이터 JPA 10. Query by Example

---
+++ demoSpringJPA_SpringDataJPA_QueryByExample/src/main/java/com/example/demospringjpa/Comment.java
@@ -0,0 +1,71 @@
+package com.example.demospringjpa;
+
+import org.hibernate.annotations.Fetch;
+
+import javax.persistence.*;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    private int up;
+
+    private int down;
+
+    private boolean best;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public int getUp() {
+        return up;
+    }
+
+    public void setUp(int up) {
+        this.up = up;
+    }
+
+    public int getDown() {
+        return down;
+    }
+
+    public void setDown(int down) {
+        this.down = down;
+    }
+
+    public boolean isBest() {
+        return best;
+    }
+
+    public void setBest(boolean best) {
+        this.best = best;
+    }
+}
